6567ecb19b97770d84b3ff13e1988894b48af5c8
shmuelbenatar <shmuelbe.at@gmail.com>
2024-11-27 14:16:02 +0200

    ooooo

--- data/starting.py
+++ data/starting.py
@@ -1,1 +1,13 @@
-print (5)
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
+# ytuhjgfdsfghjklkjhgfdsfghjk
+# Load datasets
+features_distances = pd.read_csv('/mnt/data/3d_distances.csv')
+features_angles = pd.read_csv('/mnt/data/angles.csv')
+labels = pd.read_csv('/mnt/data/labels.csv')
