9d3dcfbdc0563f96449f449362bfbc97017320f1
omero <omer.apter@msmail.ariel.ac.il>
2024-12-25 13:10:44 +0200

    שיםור המודל הרגרסי עם הוספת פיצ'רים. הדפסת הפסד ודיוק.

--- simple_model.py
+++ simple_model.py
@@ -43,30 +43,40 @@ class SoftmaxClassifier(nn.Module):
 num_of_classes = 10
 num_epochs = 10000
 train_split = 0.8
+learning_rate = 0.0001
 # end of constants
 
-# הבאת המידע מאקסל
+# הבאת המידע מאקסל ואיחוד פיצ'רים
 distances_3d = pd.read_csv('data/3d_distances.csv', skip_blank_lines=True)
-distances_3d = distances_3d.drop('pose_id', axis=1)
+angles = pd.read_csv('data/angles.csv', skip_blank_lines=True)
+xyz_distances = pd.read_csv('data/xyz_distances.csv', skip_blank_lines=True)
 labels = pd.read_csv('data/labels.csv', skip_blank_lines=True)
+
+data = distances_3d.join(angles.set_index('pose_id'), on='pose_id', how='left')
+data = data.join(xyz_distances.set_index('pose_id'), on='pose_id', how='left')
+
+
+# עיבוד מידע להתאים ללמידה
+data = data.drop('pose_id', axis=1)
 labels = labels.drop('pose_id', axis=1)
 labels_tensor = li.create_label_tensor(num_of_classes, labels, 'pose')
 # labels_indexed = li.convert_df_labels(labels, 'pose')
-dis3d_tensor = df_to_tensor(distances_3d)
-dataset = torch.utils.data.TensorDataset(dis3d_tensor, labels_tensor)
+data_tensor = df_to_tensor(data)
+dataset = torch.utils.data.TensorDataset(data_tensor, labels_tensor)
 # labels_t = df_to_tensor(labels_indexed)
 
-num_of_features = distances_3d.columns.size
+num_of_features = data.columns.size
 
 # פיצול המידע לtest ו- train
-generator1 = torch.Generator().manual_seed(495)
+generator1 = torch.Generator()
+generator1.manual_seed(495)
 train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_split, 1-train_split], )
 # train_dataset[:][0] the features || train_dataset[:][1] the labels
 
 model = SoftmaxClassifier(num_of_features, num_of_classes)
 
 loss_fn = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.0001)
+optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 
 for epoch in range(num_epochs):
     optimizer.zero_grad()
@@ -78,7 +88,15 @@ for epoch in range(num_epochs):
     optimizer.step()
     if epoch % 1000 == 0:
         print(f'Epoch {epoch}.')
+
+
+# getting results
 with torch.no_grad():
+    targets = torch.argmax(test_dataset[:][1], dim=1)
     test_logits = model(test_dataset[:][0])
     loss = loss_fn(test_logits, test_dataset[:][1])
-    print(f"this is the loss of test: {loss.item()}")
+    ans = torch.argmax(test_logits, dim=1)
+    corrects = (ans == targets)
+    accuracy = corrects.sum().float() / float(targets.size(0))
+    print(f"this is the loss of test: {round(loss.item(), 4)}")
+    print(f"this is the accuracy of test: {round(accuracy.item(), 4)}")
